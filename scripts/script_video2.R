95e825c1be1a382d2f2391c89e841ff4f596f778
Andrew Stewart <drandrewjstewart@gmail.com>
2020-08-12 15:27:29 +0100

    add 2x2 graphs

--- scripts/script_video2.R
+++ scripts/script_video2.R
@@ -6,6 +6,32 @@ library(performance) # check model assumptions
 library(arm) # for binned residuals plot
 
 # Let's look at a 2 x 2 design
+factorial_data <- read_csv("https://raw.githubusercontent.com/ajstewartlang/15_mixed_models_pt1/master/data/2x2.csv")
+
+tidied_factorial_data <- factorial_data %>%
+  transmute(subject = factor(Subject), item = factor(Item), RT = RT,
+            context = factor(Context), sentence = factor(Sentence))
+
+tidied_factorial_data %>%
+  group_by(sentence, context) %>%
+  summarise(mean_rt = mean(RT), sd_rt = sd(RT))
+
+tidied_factorial_data %>%
+  filter(!is.na(RT)) %>%
+  group_by(sentence, context) %>%
+  summarise(mean_rt = mean(RT), sd_rt = sd(RT))
+
+tidied_factorial_data %>%
+  filter(!is.na(RT)) %>%
+  ggplot(aes(x = sentence:context, y = RT, colour = sentence:context)) +
+  geom_violin() +
+  geom_jitter(width = .1, alpha = .2) +
+  stat_summary(fun.data = "mean_cl_boot", colour = "black") +
+  guides(colour = FALSE) +
+  labs(x = "Sentence X Context",
+       y = "RT (ms.") +
+  theme_minimal() +
+  coord_flip()
 
 
 
@@ -39,4 +65,5 @@ binomial_model_null <- glmer(DV ~ (1 | subject),
 
 anova(binomial_model, binomial_model_null)
 
+# We would expect 95# of residuals to fall between jagged line (+- 2SEs)
 binnedplot(fitted(binomial_model), resid(binomial_model,type="response"))
